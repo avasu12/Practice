1f19e90d2ad480b50dc851c6b5064265ec1d5488
Ajay Vasu <ajay_vasu@outlook.com>
2024-07-25 17:17:50 +0530

    drew stuff with turtles

---
+++ draw_stuff.py
@@ -0,0 +1,19 @@
+from turtle import *
+# import emoji
+
+
+# print(emoji.emojize(":thumbs_up:"))
+
+color("blue")
+width(3)
+
+forward(100)
+left(120)
+forward(100)
+left(120)
+forward(100)
+
+circle(10)
+
+
+mainloop()
