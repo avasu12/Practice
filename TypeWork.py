de1659325893623462f5ff3bdeecbe6e8656597b
Ajay Vasu <ajay_vasu@outlook.com>
2024-08-28 22:26:29 +0530

    Miscellaneous practice

--- TypeWork.py
+++ TypeWork.py
@@ -14,3 +14,18 @@ n.test()
 x = memoryview(bytes(10))
 print(x)
 print("test commit")
+
+# type hints
+
+def database_connect(username: str):
+    print("In Database Connect")
+
+database_connect('string123')
+
+# "With" keyword
+
+with open('file.txt', 'r') as x:
+    text_collection = x.readlines()
+
+for sentence in text_collection:
+    print(sentence)
