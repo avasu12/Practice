6f4b34248ca548adf2eb78945a58e7d9818c1b37
Ajay Vasu <ajay_vasu@outlook.com>
2024-09-12 23:49:48 +0530

    Type hints with colons and arrows

--- function_defs.py
+++ function_defs.py
@@ -7,3 +7,10 @@ def fibonacci_default(start_element, end_element = 9):
     print("test message")
 
 fibonacci_default(start_element=5)
+
+def test_function(parameter1 = 3, parameter2: int = 7) -> int:
+    print("Inside test function")
+    return parameter2
+
+x = test_function()
+print(x)
