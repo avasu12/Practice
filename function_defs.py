a6a733fa182884db6c1491228ee56039014d772d
Ajay Vasu <ajay_vasu@outlook.com>
2024-09-12 23:39:39 +0530

    Function signatures

---
+++ function_defs.py
@@ -0,0 +1,9 @@
+def fibonacci(start_element, end_element):
+    print("test message")
+
+fibonacci(start_element=6, end_element=5)
+
+def fibonacci_default(start_element, end_element = 9):
+    print("test message")
+
+fibonacci_default(start_element=5)
