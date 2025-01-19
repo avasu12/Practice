aff93f019ac5a508df0a5bae1e87c61052d26cc0
Ajay Vasu <ajay_vasu@outlook.com>
2025-01-19 23:45:56 +0530

    More SQL practice

--- practice.sql
+++ practice.sql
@@ -1,3 +1,22 @@
 select name from city where countrycode = 'JPN';
 select distinct city from station where ID % 2 = 0;
 select count(city) - count(distinct city) from station;
+
+select city, length(city) from station order by length(city) desc limit 1;
+select city, length(city) from station order by length(city), city asc limit 1;
+
+/*
+SQL Query order of execution:
+
+FROM
+JOIN ON
+WHERE
+GROUP BY
+HAVING
+
+SELECT
+ORDER BY
+LIMIT
+
+*/
+
