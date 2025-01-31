f2f004ff903651b927f12b9b4f981cea3aaf0f23
Ajay Vasu <ajay_vasu@outlook.com>
2025-01-31 22:35:04 +0530

    Regular SQL practice

--- practice.sql
+++ practice.sql
@@ -9,6 +9,11 @@ select count(movie)
 from oscar_nominees
 where nominee = 'Abigail Breslin';
 
+select hotel_name,reviewer_score, count(*) 
+from hotel_reviews
+where hotel_name = 'Hotel Arena'
+group by hotel_name, reviewer_score;
+
 /*
 SQL Query order of execution:
 
