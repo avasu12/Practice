639c4141989662e7a567fb5995658f8d2a8768b9
Ajay Vasu <ajay_vasu@outlook.com>
2025-02-01 22:19:09 +0530

    Simple plot example

--- plotgraph.py
+++ plotgraph.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-
+'''
 x = np.arange(0, 10)  
 y = np.random.random(10) 
 
@@ -12,4 +12,12 @@ plt.xlabel('X-Axis')
 plt.ylabel('Y-Axis')
 plt.title('Random Line Chart')
 
+plt.show()
+'''
+
+# To "plot" means to draw/mark points on a graph
+x = [1, 2, 3, 4, 5, 6, 7]
+y = [20, 30, 20, 10, 5, 2, 1]
+
+plt.plot(x, y)
 plt.show()
