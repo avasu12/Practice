451ee0e284302253fb43a915bcfb5bd4bceec12a
Ajay Vasu <ajay_vasu@outlook.com>
2025-02-05 23:39:20 +0530

    Array creation practice

--- numeric.py
+++ numeric.py
@@ -2,3 +2,19 @@ import numpy as np
 
 a = np.array([[3, 6, 9], [9, 36, 81]])
 print(a)
+
+print(a.shape)
+print(a.ndim)
+print(a.size)
+
+zero_array = np.zeros(5)
+print(zero_array)
+
+one_array = np.ones(8)
+print(one_array)
+
+empty_array = np.empty(6)
+print(empty_array)
+
+arange_array = np.arange(4)
+print(arange_array)
