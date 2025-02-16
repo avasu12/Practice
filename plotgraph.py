747cb790174b832224995e1298b65ddcd82d9115
Ajay Vasu <ajay_vasu@outlook.com>
2025-02-16 23:52:18 +0530

    Matplotlib figures

--- plotgraph.py
+++ plotgraph.py
@@ -2,5 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-plt.plot([1,2,3,4,5,6,7], [23,45,33,57,1,66,54])
+# Figure
+# Axes
+
+'''
+figure, axes = plt.subplots()
+axes.plot([1,2,3,4,5], [5,7,8,9,1])
+plt.show()
+'''
+
+figure = plt.figure()
 plt.show()
