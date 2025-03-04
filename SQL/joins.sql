0359b0f3b17f4e3c9504fa09e5dcc06b730d19b3
Ajay Vasu <ajay_vasu@outlook.com>
2025-03-04 09:21:05 +0530

    Stratascratch medium solution

--- SQL/joins.sql
+++ SQL/joins.sql
@@ -6,3 +6,13 @@ left join premium_accounts_by_day as b
 on a.entry_date+7 = b.entry_date
 where a.final_price <> 0
 group by a.entry_date;
+
+select a.entry_date, count(a.account_id) as prem_accts, count(b.account_id) as still_premium
+from premium_accounts_by_day as a
+left join premium_accounts_by_day as b
+on a.account_id = b.account_id
+and b.entry_date - a.entry_date = 7 and b.final_price > 0
+where a.final_price > 0
+group by a.entry_date
+order by a.entry_date
+limit 7;
