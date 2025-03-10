8b808a7ecf68f055ed732ed159368c5ea1a1612c
Ajay Vasu <ajay_vasu@outlook.com>
2025-03-10 22:43:48 +0530

    Practice Questions

--- SQL/stratascratch.sql
+++ SQL/stratascratch.sql
@@ -1,3 +1,15 @@
-/* */
+/* 
+Medium - Google
+*/
 
-
+with mytable as(
+    select concat(user_firstname, ' ', user_lastname) as full_name, count(distinct video_id) as total_approved
+    from user_flags as a
+    inner join flag_review as b on a.flag_id = b.flag_id
+    where b.reviewed_by_yt = true and b.reviewed_outcome = 'APPROVED'
+    group by full_name
+    order by total_approved desc
+)
+select full_name
+from mytable
+where total_approved = (select max(total_approved) from mytable);
