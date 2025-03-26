62db8d348279139233110eb49a309c5203460c58
Ajay Vasu <ajay_vasu@outlook.com>
2025-03-26 15:56:38 +0530

    introduction to pandas

--- pandas_practice.py
+++ pandas_practice.py
@@ -1,4 +1,14 @@
 import pandas as pd
 
-s = pd.Series({"a": 1, "b": 2})
-print(s)
+'''
+Create data structures
+'''
+
+test_structure = pd.Series([98, 45, 78, 92, 41, 79])
+
+
+'''
+View data
+'''
+test_structure.head()
+test_structure.tail()
