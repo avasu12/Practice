f6557893a5cdbf181becc5c3e3d7845128a2c28d
Ajay Vasu <ajay_vasu@outlook.com>
2025-03-27 11:03:28 +0530

    Python data structures

--- TypeWork.py
+++ TypeWork.py
@@ -1,53 +1,9 @@
-import pandas as pd
-from typing import List, Dict, Optional
+'''
+Data Structures - how information is arranged/organized and related
+Data Types
 
+'''
 
-data_frame = pd.DataFrame({'test': [1,4,6,7,9], 'test2': [2,3,5, 0,1]})
-print(data_frame)
-print(data_frame['test2'])
-print(data_frame.describe())
-print(data_frame['test2'].sum())
-data_frame2 = pd.DataFrame({'Fruits': ['dragonfruit', 'mango', 'papaya', 'jackfruit'], 'Vegetables': ['Asparagus', 'Brussel Sprouts', 'Carrots', 'Beans'], 'test2': [3, 5, 3, 0]})
-print(data_frame2)
-frames = [data_frame2, data_frame]
-test = pd.concat(frames)
-print(test)
-testjoin = pd.merge(data_frame, data_frame2, on="test2")
-print(testjoin)
-
-
-class Node:
-    def __init__(self):
-        print('Object Initialized')
-
-    def test(self):
-        print('Inside class Node')
-
-n = Node()
-
-# print(n)
-n.test()
-
-x = memoryview(bytes(10))
-# print(x)
-# print("test commit")
-
-# type hints
-
-def database_connect(username: str):
-    print("In Database Connect")
-
-database_connect('string123')
-
-def complex_hint(parameter: List[Dict[str, Optional[int]]]):
-    print("In Compelx Hint")
-
-complex_hint([{"data": 3}])
-
-# "With" keyword
-
-with open('file.txt', 'r') as x:
-    text_collection = x.readlines()
-
-for sentence in text_collection:
-    print(sentence)
+test_list = [1,3,4,6,9]
+test_list.append(19)
+print(test_list)
