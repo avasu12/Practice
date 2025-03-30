d5413d868afb0460bf32bd3a151f22048ff5e3a9
Ajay Vasu <ajay_vasu@outlook.com>
2025-03-30 10:22:38 +0530

    Basic Types in Python

--- TypeWork.py
+++ TypeWork.py
@@ -2,8 +2,19 @@
 Data Structures - how information is arranged/organized and related
 Data Types
 
+Boolean
+Numeric
+
+Sequence
+Text Sequence
+Binary Sequence
+
+Mapping
+
+
 '''
 
-test_list = [1,3,4,6,9]
-test_list.append(19)
-print(test_list)
+
+# Boolean
+x = bool("false")
+print(x)
