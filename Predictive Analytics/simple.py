fdd6bf4ab9f2b6809d3ad77ddcc3a0d797139606
Ajay Vasu <ajay_vasu@outlook.com>
2025-07-01 22:50:32 +0530

    Predictive Analytics initiation

---
+++ Predictive Analytics/simple.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+data = {
+    'TV Advertising': [230, 44, 17, 151, 180, 89, 200],
+    'Sales': [22, 10, 9, 18, 20, 14, 21]
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+X = df[['TV Advertising']]
+y = df['Sales']
+
+model = LinearRegression()
+model.fit(X, y)
+
+print("Slope: ", model.coef_[0])
+print("Intercept: ", model.intercept_)
