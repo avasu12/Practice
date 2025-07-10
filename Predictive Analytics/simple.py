38006d05c9639dbb591b4f5827f689e8eedd5a70
Ajay Vasu <ajay_vasu@outlook.com>
2025-07-10 18:16:57 +0530

    Dataframe creation

--- Predictive Analytics/simple.py
+++ Predictive Analytics/simple.py
@@ -1,7 +1,21 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-rev_dataframe = pd.read_csv("filepath")
+rev_dataframe = pd.read_csv("~/Desktop/numbers.csv")
 print(rev_dataframe)
 print(rev_dataframe.dtypes)
 print("Sum: ", rev_dataframe['Revenue'].sum())
+
+date_series = pd.to_datetime(rev_dataframe['Date'])
+number_date_series = date_series.astype(int)/ 10**9
+print(number_date_series)
+
+converted_frame = pd.DataFrame({
+    'Dates': number_date_series,
+    'Revenue': rev_dataframe['Revenue']
+})
+
+print(converted_frame)
+
+
+
